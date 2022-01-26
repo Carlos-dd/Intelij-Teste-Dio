54224ccfdf742d7bbe6d76abc3faa7e74b8900d1
Carlos-dd <cardudu2013@gmail.com>
2022-01-26 20:21:47 -0300

    nova class livro

---
+++ src/br/com/dio/Livro.java
@@ -0,0 +1,6 @@
+package br.com.dio;
+
+public class Livro {
+    public Livro(Object p0, String s) {
+    }
+}
--- src/br/com/dio/PrimeiroProgram.java
+++ src/br/com/dio/PrimeiroProgram.java
@@ -6,9 +6,41 @@ public class PrimeiroProgram {
     public static void main(String[] args) {
         Gato gato = new Gato();
         System.out.println(gato);
-        /*int a = 5;
-             int b = 2;
 
-        System.out.println(a +  b);*/
+        Livro livro1 = new Livro("neuromancer", "20");
+
+            }
+}
+class livro {
+    private String nome;
+    private Integer numPag;
+
+    public livro(String nome, String numPag) {
+        this.nome = nome;
+        this.numPag = Integer.valueOf(numPag);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
