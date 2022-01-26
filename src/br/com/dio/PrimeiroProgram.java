e3fdfeaf2012e8242fd5f23f07f8650f25cc8107
Carlos-dd <cardudu2013@gmail.com>
2022-01-26 19:53:08 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroProgram.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProgram {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+        /*int a = 5;
+             int b = 2;
+
+        System.out.println(a +  b);*/
+    }
+}
